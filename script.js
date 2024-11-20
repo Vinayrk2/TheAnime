4438da8db5125922de6162234937b40d5fe6430d
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2024-11-20 19:12:02 +0530

    Changed the way of look

--- script.js
+++ script.js
@@ -3,28 +3,25 @@ console.log("connected");
 let currentType = "sfw";
 
 function loadNewImage(type, category) {
-  fetch(`https://api.waifu.pics/${type}/${category}`, { method: "get" })
+  fetch(`https://api.waifu.pics/sfw/${category}`, { method: "get" })
     .then((data) => {  return data.json(); })
     .then((processedData) => { document.getElementById("cardImage").setAttribute("src",processedData.url.toString()); document.getElementById("cardButton").setAttribute("href",processedData.url) })
 }
 
 const type = ['sfw']
-const category = [['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug', 'awoo', 'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite', 'glomp', 'slap', 'kill', 'kick', 'happy', 'wick', 'poke', 'dance', 'cringe'],
-['waifu', 'neko', 'trap', 'blowjob']
+const category = [['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug', 'awoo', 'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite', 'glomp', 'slap', 'kill', 'kick', 'happy', 'wick', 'poke', 'dance', 'cringe']
 ]
 
-function changeCategory(value) {
+function changeCategory() {
   let categoryIndex = 0;
+  currentType = 0;
 
-  value == 'sfw' ? categoryIndex = 0 : categoryIndex = 1;
-  currentType = value;
-
-  
   document.getElementById("category").innerHTML = category[categoryIndex].map((val) => {
     return `<option value='${val}'> ${val} </option>`
   })
 }
 
+changeCategory()
 function loadCardImage(value) {
   document.getElementById("cardTitle").innerText = value.toString();
   document.getElementById("cardImage").setAttribute("src","loading.gif")
